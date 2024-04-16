08a2c0ce18ff8f7c76de566c60b42ba2c6e9bf1a
Adilet97 <anurllanov@gmail.com>
2024-04-16 19:29:28 +0600

    Initial commit

---
+++ src/Dog.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+
+public class Dog extends Pet {
+    private String name;
+    private String breed;
+    private String[] commands;
+
+    public Dog(String name, String breed, String[] commands,
+               Color color, Shelter shelter) {
+        this.name = name;
+        this.breed = breed;
+        this.commands = commands;
+        super.setColor(color);
+        super.setShelter(shelter);
+    }
+
+    public Dog(String name, String breed,
+               Color color, Shelter shelter) {
+        this.name = name;
+        this.breed = breed;
+        super.setColor(color);
+        super.setShelter(shelter);
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String[] getCommands() {
+        return commands;
+    }
+
+    public void setCommands(String commands) {
+        this.commands = new String[]{commands};
+    }
+    public void makeVoice(){
+        System.out.println("Собака издает звук");
+    }
+
+    public void makeVoice(String voice) {
+        System.out.println("Собака издает звук" + voice);
+    }
+    public String getInfo(){
+        return super.getInfo()+
+                "\nName: "+name+
+                "\nBreed" + breed+
+                "\nCommands" + Arrays.toString(commands);
+}}
---
+++ src/Shelter.java
@@ -0,0 +1,17 @@
+public class Shelter {
+    private String name;
+    private int address;
+
+    public Shelter(String name, int address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAddress() {
+        return address;
+    }
+}
